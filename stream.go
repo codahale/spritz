6537339933e810ea177585f3f5c7c4e1e12abe08
Coda Hale <coda.hale@gmail.com>
2014-11-08 13:46:52 -0800

    Added support for IVs.

--- stream.go
+++ stream.go
@@ -4,6 +4,12 @@ import "crypto/cipher"
 
 // NewStream returns a new instance of the Spritz cipher using the given key.
 func NewStream(key []byte) cipher.Stream {
+	return NewStreamWithIV(key, nil)
+}
+
+// NewStreamWithIV returns a new instance of the Spritz cipher using the given
+// key and initialization vector.
+func NewStreamWithIV(key, iv []byte) cipher.Stream {
 	var s state
 	s.initialize(256)
 
@@ -12,7 +18,10 @@ func NewStream(key []byte) cipher.Stream {
 	if s.a > 0 {
 		s.shuffle()
 	}
-
+	if iv != nil {
+		s.absorbStop()
+		s.absorbBytes(iv)
+	}
 	return stream{s: &s}
 }
 
