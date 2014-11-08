3dbaa2f481534c5b66fc3f45d88b8f288d7f7719
Coda Hale <coda.hale@gmail.com>
2014-11-08 10:30:40 -0800

    Throwing my hat into a dumb ring again.

--- spritz.go
+++ spritz.go
@@ -29,6 +29,43 @@ func NewHash(size int) hash.Hash {
 	return d
 }
 
+// InsecurePasswordHash calculates a CPU- and memory-hard hash of the given
+// password and salt. It takes an exponential parameter, m, which determines
+// both CPU and memory cost. It also takes the length of the hash in bytes.
+//
+// N.B.: THIS IS A TOTALLY EXPERIMENTAL ALGORITHM WHICH I WROTE BEFORE I'D HAD
+// ANY COFFEE. DO NOT USE HACKY ALGORITHMS DESIGNED BY UNCAFFEINATED
+// NON-CRYPTOGRAPHERS.
+func InsecurePasswordHash(password, salt []byte, m, n uint) []byte {
+	var s state
+
+	// initialize to 2**m bytes
+	s.initialize(1 << m)
+
+	// absorb the password
+	s.absorbBytes(password)
+	if s.a > 0 {
+		s.shuffle()
+	}
+	s.absorbStop()
+
+	// absorb the salt
+	s.absorbBytes(salt)
+	if s.a > 0 {
+		s.shuffle()
+	}
+	s.absorbStop()
+
+	// absorb the length
+	s.absorbValue(int(n))
+	s.absorbStop()
+
+	// squeeze out the digest
+	out := make([]byte, int(n))
+	s.squeezeBytes(out)
+	return out
+}
+
 type stream struct {
 	s *state
 }
--- spritz_test.go
+++ spritz_test.go
@@ -53,6 +53,60 @@ func TestHash(t *testing.T) {
 	}
 }
 
+func BenchmarkPasswordHash15(b *testing.B) {
+	v := []byte("hello this is a password")
+	b.ReportAllocs()
+
+	for i := 0; i < b.N; i++ {
+		spritz.InsecurePasswordHash(v, nil, 15, 32)
+	}
+}
+
+func BenchmarkPasswordHash16(b *testing.B) {
+	v := []byte("hello this is a password")
+	b.ReportAllocs()
+
+	for i := 0; i < b.N; i++ {
+		spritz.InsecurePasswordHash(v, nil, 16, 32)
+	}
+}
+
+func BenchmarkPasswordHash17(b *testing.B) {
+	v := []byte("hello this is a password")
+	b.ReportAllocs()
+
+	for i := 0; i < b.N; i++ {
+		spritz.InsecurePasswordHash(v, nil, 17, 32)
+	}
+}
+
+func BenchmarkPasswordHash18(b *testing.B) {
+	v := []byte("hello this is a password")
+	b.ReportAllocs()
+
+	for i := 0; i < b.N; i++ {
+		spritz.InsecurePasswordHash(v, nil, 18, 32)
+	}
+}
+
+func BenchmarkPasswordHash19(b *testing.B) {
+	v := []byte("hello this is a password")
+	b.ReportAllocs()
+
+	for i := 0; i < b.N; i++ {
+		spritz.InsecurePasswordHash(v, nil, 19, 32)
+	}
+}
+
+func BenchmarkPasswordHash20(b *testing.B) {
+	v := []byte("hello this is a password")
+	b.ReportAllocs()
+
+	for i := 0; i < b.N; i++ {
+		spritz.InsecurePasswordHash(v, nil, 20, 32)
+	}
+}
+
 func BenchmarkStream(b *testing.B) {
 	v := []byte{'a', 'r', 'c', 'f', 'o', 'u', 'r'}
 	s := spritz.NewStream(v)
