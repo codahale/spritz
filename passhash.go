b2bf188d88f94ed368de197a8ade77febb269e72
Coda Hale <coda.hale@gmail.com>
2014-11-09 18:25:44 -0800

    Add both memory and CPU cost.

--- passhash.go
+++ passhash.go
@@ -1,22 +1,20 @@
 package spritz
 
 // InsecurePasswordHash calculates a CPU- and memory-hard hash of the given
-// password and salt. It takes a linear parameter, m, which determines both CPU
-// and memory cost. It also takes the length of the hash in bytes.
+// password and salt. It takes two exponential parameters, M and T, which
+// determine the memory and CPU costs. It also takes the length of the hash in
+// bytes.
 //
 // N.B.: THIS IS A TOTALLY EXPERIMENTAL ALGORITHM WHICH I WROTE BEFORE I'D HAD
 // ANY COFFEE. DO NOT USE HACKY ALGORITHMS DESIGNED BY UNCAFFEINATED
 // NON-CRYPTOGRAPHERS.
-func InsecurePasswordHash(password, salt []byte, m, n int) []byte {
-	// initialize to 256*m bytes
+func InsecurePasswordHash(password, salt []byte, m, t, n uint) []byte {
+	// initialize to 256*(2**m) bytes
 	var s state
-	s.initialize(256 * m)
+	s.initialize(1 << (m + 8))
 
 	// absorb the password
 	s.absorb(password)
-	if s.a > 0 {
-		s.shuffle()
-	}
 
 	// absorb the salt
 	s.absorbStop()
@@ -25,6 +23,11 @@ func InsecurePasswordHash(password, salt []byte, m, n int) []byte {
 	// absorb the length
 	s.absorbStop()
 	s.absorbByte(int(n))
+
+	// iterate through 2**t bytes of output
+	for i := 0; i < 1<<uint(t); i++ {
+		s.drip()
+	}
 
 	// squeeze out the digest
 	out := make([]byte, n)
--- passhash_test.go
+++ passhash_test.go
@@ -6,29 +6,11 @@ import (
 	"github.com/codahale/spritz"
 )
 
-func BenchmarkPasswordHash256(b *testing.B) {
+func BenchmarkPasswordHash(b *testing.B) {
 	v := []byte("hello this is a password")
 	b.ReportAllocs()
 
 	for i := 0; i < b.N; i++ {
-		spritz.InsecurePasswordHash(v, nil, 256, 32)
-	}
-}
-
-func BenchmarkPasswordHash512(b *testing.B) {
-	v := []byte("hello this is a password")
-	b.ReportAllocs()
-
-	for i := 0; i < b.N; i++ {
-		spritz.InsecurePasswordHash(v, nil, 512, 32)
-	}
-}
-
-func BenchmarkPasswordHash1024(b *testing.B) {
-	v := []byte("hello this is a password")
-	b.ReportAllocs()
-
-	for i := 0; i < b.N; i++ {
-		spritz.InsecurePasswordHash(v, nil, 1024, 32)
+		spritz.InsecurePasswordHash(v, nil, 10, 11, 32)
 	}
 }
